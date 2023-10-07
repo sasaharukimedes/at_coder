e9ec0054572a06732c9c0472cbf898267e73dae7
sasaharukimedes <sasaharu3@gmail.com>
2023-10-07 18:51:51 +0900

    add problem

---
+++ at_coder_problems/training/easy/3.rb
@@ -0,0 +1,21 @@
+n, a, b = gets.split.map(&:to_i)
+s = gets.chomp
+
+na = 0
+nb = 0
+
+for i in 0...n
+  yosen = 'No'
+  if s[i] == 'a'
+    if na + nb < a + b
+      na += 1
+      yosen = 'Yes'
+    end
+  elsif s[i] == 'b'
+    if na + nb < a + b && nb < b
+      nb += 1
+      yosen = 'Yes'
+    end
+  end
+  puts yosen
+end
