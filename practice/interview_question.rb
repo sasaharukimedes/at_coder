b702ec7041346cc76def6fa3c5e2e02164cc7169
sasaharukimedes <sasaharu3@gmail.com>
2023-11-06 22:47:24 +0900

    add intereview in practice

---
+++ practice/interview_question.rb
@@ -0,0 +1,19 @@
+class CelebratingBirthday
+  # クラスメソッドで誕生日を検索
+  def self.birthday(target_date)
+    birthdays = [["山田", "04-01"], ["田中", "12-31"]]
+    birthdays.each do |name, date|
+      return name if date == target_date
+    end
+    nil  # 該当する誕生日が見つからない場合
+  end
+end
+
+# 既存のbirthdays変数を使用してクラスメソッドを呼び出す
+result = CelebratingBirthday.birthday("04-01")
+
+if result
+  puts result
+else
+  puts "該当する誕生日が見つかりません。"
+end
