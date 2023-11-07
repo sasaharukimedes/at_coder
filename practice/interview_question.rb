5b86232e6b2d9f06d1080bdc3a8960a908c0cd27
sasaharukimedes <sasaharu3@gmail.com>
2023-11-07 21:54:08 +0900

    add cheeting_sheets folder

--- practice/interview_question.rb
+++ practice/interview_question.rb
@@ -3,6 +3,7 @@ class CelebratingBirthday
   def self.birthday(target_date)
     birthdays = [["山田", "04-01"], ["田中", "12-31"]]
     birthdays.each do |name, date|
+      puts "name:#{name}, date:#{date}"
       return name if date == target_date
     end
     nil  # 該当する誕生日が見つからない場合
@@ -10,7 +11,7 @@ class CelebratingBirthday
 end
 
 # 既存のbirthdays変数を使用してクラスメソッドを呼び出す
-result = CelebratingBirthday.birthday("04-01")
+result = CelebratingBirthday.birthday("12-31")
 
 if result
   puts result
