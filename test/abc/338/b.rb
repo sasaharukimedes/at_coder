8d654ebbaf6ef68839ebfa1aa0840a0a4cf9c142
sasaharukimedes <sasaharu3@gmail.com>
2024-02-14 22:51:33 +0900

    add folder

--- test/abc/338/b.rb
+++ test/abc/338/b.rb
@@ -7,3 +7,16 @@ max_frequency = frequency.values.max
 most_frequent_chars = frequency.select { |_, v| v == max_frequency }.keys
 
 puts most_frequent_chars.sort.first
+
+
+
+#officails answer
+
+S = gets.chomp
+a = Hash.new(0)
+S.each_char { |c| a[c] += 1 }
+ans = 'a'
+('b'..'z').each do |c|
+  ans = c if a[c] > a[ans]
+end
+puts ans
--- test/abc/338/c.rb
+++ test/abc/338/c.rb
@@ -2,5 +2,18 @@ N = gets.to_i
 Q = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
 B = gets.split.map(&:to_i)
+INF = 10**18
 
-
+ans = 0
+(0..Q.max).each do |x|
+  y = INF
+  N.times do |i|
+    if Q[i] < A[i] * x
+      y = -INF
+    elsif B[i] > 0
+      y = [y, (Q[i] - A[i] * x) / B[i]].min
+    end
+  end
+  ans = [ans, x + y].max
+end
+puts ans
