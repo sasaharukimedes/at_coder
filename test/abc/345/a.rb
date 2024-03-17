fec87335a0eddf8878b47dae3591bc1f073acefc
sasaharukimedes <sasaharu3@gmail.com>
2024-03-17 18:50:01 +0900

    add markdown

--- test/abc/345/a.rb
+++ test/abc/345/a.rb
@@ -5,3 +5,19 @@ if s[0] == '<' && s[-1] == '>' && s[1..-2] == '=' * (s.length - 2)
 else
   puts 'No'
 end
+---
+s = gets.chomp
+flag = true
+
+flag = false if s[0] != '<'
+flag = false if s[-1] != '>'
+
+(1..s.size - 2).each do |i|
+  flag = false if s[i] != '='
+end
+
+if flag
+  puts "Yes"
+else
+  puts "No"
+end
--- test/abc/345/b.rb
+++ test/abc/345/b.rb
@@ -3,3 +3,6 @@ x = gets.to_i
 result = (x / 10.0).ceil
 
 puts result
+
+#丸め誤差が意味わからん
+
